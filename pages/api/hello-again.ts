58e0092a8b6ffabecadf40d2b50b8e1245cbdc17
Sergey Mount <i@b9sk.ru>
2021-10-09 01:00:14 +0300

    create sample data and get it back

---
+++ pages/api/hello-again.ts
@@ -0,0 +1,53 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+type Data = {
+  email: string | null
+  name: string | null
+}
+
+// Init Prisma
+const prisma = new PrismaClient()
+
+async function fetchUsers() {
+  return await prisma.user.findMany({
+    select: {
+      id: false,
+      name: true,
+      email: true,
+    }
+  })
+}
+
+async function createSampleUser(): Promise<void> {
+  await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "jd@mail.com",
+      profile: {
+        create: {
+          bio: "I like cats"
+        }
+      }
+    }
+  })
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data[]>
+) {
+  let users
+  users = await fetchUsers()
+
+  // if the result has no users then create a user and fetch the data again
+  if (users.length === 0) {
+    await createSampleUser()
+    users = await fetchUsers()
+  }
+
+  console.log(users);
+
+  res.status(200).json( users )
+}
