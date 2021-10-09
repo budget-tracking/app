89dc8ae4ce85a006d40ea756feb79f496a433c68
Sergey Mount <i@b9sk.ru>
2021-10-10 02:15:35 +0300

    add nextauth github provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import EmailProvider from "next-auth/providers/email"
-import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
@@ -13,16 +13,16 @@ export default NextAuth({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID ?? "",
-      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
   jwt: {
     secret: process.env.SECRET
   },
   session: {
-    jwt: true
+    jwt: true,
   },
   debug: true
 })
