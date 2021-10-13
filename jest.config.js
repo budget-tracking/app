7eade2e63e4b1913511ee8fbf327840bd23dad52
Sergey Mount <i@b9sk.ru>
2021-10-13 23:34:11 +0300

    update jest config; add test.sh for running tests inside docker

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,20 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  globals: {
+      "ts-jest": {
+          tsconfig: "tsconfig.json"
+      }
+  },
+  moduleFileExtensions: [
+      "ts",
+      "js"
+  ],
+  transform: {
+      "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  testMatch: [
+      // "**/test/**/*.test.(ts|js)",
+      "**/*.test.(ts|js)",
+  ],
+  testEnvironment: "node"
 };
