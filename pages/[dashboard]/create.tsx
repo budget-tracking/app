52e1ee82eef73a0a8f19ffb2ff8fad4281eafd09
Sergey Mount <i@b9sk.ru>
2021-10-14 06:46:16 +0300

    make a "create entry" form

--- pages/[dashboard]/create.tsx
+++ pages/[dashboard]/create.tsx
@@ -1,36 +1,72 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/dist/client/router'
-import React, { BaseSyntheticEvent } from 'react'
+import React, { BaseSyntheticEvent, useState } from 'react'
 
 interface Props { }
+
+type Form = {
+  description: string
+  amount: string
+  type: string
+}
 
 function Create(props: Props) {
   const { } = props
   const router = useRouter()
-  const {data: session, status} = useSession()
+  const { data: session, status } = useSession()
+  const [form, setForm] = useState<Form>({
+    amount: "",
+    description: "",
+    type: ""
+
+  })
   if (status === 'loading') return null
   if (!session) router.push('/')
 
   const handleFormSubmit = (e: BaseSyntheticEvent) => {
     e.preventDefault()
-    console.log(e.target.constructor.name);
-    
-    const formData = new FormData(e.target)
-    for (const item in formData.entries()) {
-      console.log({item});
-      
-    }
-    // console.log();
-    
-    
+    console.log(form);
+    // TODO make request to POST /api/dashboard/create
+  }
+
+  const handleFormChange = (e: BaseSyntheticEvent) => {
+    console.log('form changed');
+    const target = e.target
+    const { name, value } = target
+    setForm({ ...form, ...{ [name]: value } })
   }
 
   return (
     <>
-      <form action="/api/dashboard/create" method="POST" onSubmit={(e) => handleFormSubmit(e)}>
-        <input type="text" name="description" id="" />
-        <input type="number" name="amount" id="" required />
-        <select name="type" id="">
+      <form
+        action="/api/dashboard/create"
+        method="POST"
+        onSubmit={(e) => handleFormSubmit(e)}
+      >
+        <input
+          type="number"
+          name="amount"
+          required
+          placeholder="Amount"
+          value={form.amount}
+          onChange={e => handleFormChange(e)}
+
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={form.description}
+          onChange={e => handleFormChange(e)}
+
+        />
+        <select
+          // TODO check if it is better to call typeName
+          name="type"
+          onChange={e => handleFormChange(e)}
+          defaultValue=""
+          required
+        >
           <option value="" disabled>select</option>
           <option value="income">Income</option>
           <option value="expense">Expense</option>
